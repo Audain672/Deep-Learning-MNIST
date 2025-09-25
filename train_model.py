9b3e6fb468a9e19b8956fee9decc84264417c645
Audain672 <melileffy@gmail.com>
2025-09-26 00:22:07 +0100

    Update training parameters and refactor MLflow integration in model training script; add .gitignore for MLflow artifacts

--- train_model.py
+++ train_model.py
@@ -1,9 +1,9 @@
 import mlflow
-import mlflow.keras
+from tensorflow import keras
 
 # --- Definition des parametres de l'experience ---
-EPOCHS = 5
-BATCH_SIZE = 128
+EPOCHS = 6
+BATCH_SIZE = 110
 VALIDATION_SPLIT = 0.1
 DROPOUT_RATE = 0.2
 DENSE_UNITS_LAYER1 = 512
@@ -30,7 +30,7 @@ with mlflow.start_run():
     mlflow.log_param("metrics", METRICS)
 
     # Chargement du jeu de donnees MNIST
-    (x_train, y_train), (x_test, y_test) = mlflow.keras.datasets.mnist.load_data()
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
     # Normalisation des donnees
     x_train = x_train.astype("float32") / 255.0
@@ -40,10 +40,10 @@ with mlflow.start_run():
     x_train = x_train.reshape(60000 , 784)
     x_test = x_test.reshape(10000 , 784)
     # Construction du modele
-    model = mlflow.keras.Sequential([
-        mlflow.keras.layers.Dense(DENSE_UNITS_LAYER1, activation ='relu', input_shape =(784, ) ),
-        mlflow.keras.layers.Dropout(DROPOUT_RATE),
-        mlflow.keras.layers.Dense(DENSE_UNITS_OUTPUT, activation ='softmax')
+    model = keras.Sequential([
+        keras.layers.Dense(DENSE_UNITS_LAYER1, activation ='relu', input_shape =(784, ) ),
+        keras.layers.Dropout(DROPOUT_RATE),
+        keras.layers.Dense(DENSE_UNITS_OUTPUT, activation ='softmax')
     ])
     # Compilation du modele
     model.compile (optimizer =OPTIMIZER, loss=LOSS_FUNCTION, metrics=METRICS)
